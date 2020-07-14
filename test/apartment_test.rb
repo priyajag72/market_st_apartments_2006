a7b51070990b877d27f831768a5bb394f1b32048
Priya Power <priyajag72@gmail.com>
2020-07-14 09:27:03 -0600

    Adds test code for monthly rent attribute.

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -13,6 +13,7 @@ class ApartmentTest < Minitest::Test
   def test_it_has_attributes
     unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
     assert_equal "A1", unit1.number
+    assert_equal 1200, unit1.monthly_rent
   end
 
 end
