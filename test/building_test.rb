270df9d3a1e448174bb8ccbc87d5c10b1763cce1
Priya Power <priyajag72@gmail.com>
2020-07-14 09:51:25 -0600

    Creates initial test and class file for building class.

---
+++ test/building_test.rb
@@ -0,0 +1,16 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/renter"
+require "./lib/apartment"
+#require "./lib/"
+
+class ClassTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
