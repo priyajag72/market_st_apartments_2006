fdfbf9fc6762c0dd7a257a06770f10093446969d
Priya Power <priyajag72@gmail.com>
2020-07-14 10:39:41 -0600

    Adds test code for  it can check units are rented.

--- test/building_test.rb
+++ test/building_test.rb
@@ -55,4 +55,21 @@ class BuildingTest < Minitest::Test
     assert_equal 1099.5, building.average_rent
   end
 
+  # ITERATION 3
+
+  def test_case_name
+    renter1 = Renter.new("Spencer")
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 1, bedrooms: 2})
+    unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 2, bedrooms: 3})
+    building = Building.new
+
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    building.add_unit(unit3)
+
+    assert_equal [], building.rented_units
+  end
+
 end
