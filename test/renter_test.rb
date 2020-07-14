d1523d874852b254f144e6928e852f0b6086c5ac
Priya Power <priyajag72@gmail.com>
2020-07-14 09:07:30 -0600

    Updates test file for require renter.rb

---
+++ test/renter_test.rb
@@ -0,0 +1,14 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/renter"
+
+# class ClassTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+# end
