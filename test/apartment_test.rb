4373bc1680e6dc4c840f2923f4894f79f8e168f0
Priya Power <priyajag72@gmail.com>
2020-07-14 09:18:14 -0600

    Adds test code for test it exists.

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -5,9 +5,9 @@ require "./lib/apartment"
 
 class ApartmentTest < Minitest::Test
 
-  #def test_it_exists
-    #assert_instance_of <Class>,
-  #end
+  def test_it_exists
+    assert_instance_of Apartment, unit1
+  end
 
   #def test_it_has_attributes
   #end
