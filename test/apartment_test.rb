90b9ee95bdd2663ee16982c5cf9ed030d8796bcd
Priya Power <priyajag72@gmail.com>
2020-07-14 09:20:07 -0600

    Updates instantiation of apartment class to include interaction pattern parameters.

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -6,7 +6,7 @@ require "./lib/apartment"
 class ApartmentTest < Minitest::Test
 
   def test_it_exists
-    unit1 = Apartment.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
     assert_instance_of Apartment, unit1
   end
 
