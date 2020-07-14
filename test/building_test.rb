7d4ac8983b1cce204d2f16831ac416b89ca3cd8a
Priya Power <priyajag72@gmail.com>
2020-07-14 09:53:58 -0600

    Adds test code for units attribute from Building class.

--- test/building_test.rb
+++ test/building_test.rb
@@ -11,7 +11,9 @@ class BuildingTest < Minitest::Test
     assert_instance_of Building, building
   end
 
-  #def test_it_has_attributes
-  #end
+  def test_it_has_attributes
+    building = Building.new
+    assert_equal [], building.units
+  end
 
 end
