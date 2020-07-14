fdc2acfd64ab86333738600ea8304bf52e6938f1
Priya Power <priyajag72@gmail.com>
2020-07-14 10:23:40 -0600

    RIssue found: renters method in building class is hard coded and will never access the rest of the units. Need enum.

--- test/building_test.rb
+++ test/building_test.rb
@@ -33,9 +33,11 @@ class BuildingTest < Minitest::Test
     unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
     building = Building.new
     building.add_unit(unit1)
-    building.add_unit(unit2)
     unit1.add_renter(renter1)
     assert_equal ["Aurora"], building.renters
+    building.add_unit(unit2)
+    unit2.add_renter(renter2)
+    assert_equal ["Aurora", "Tim"], building.renters
   end
 
 end
